bd1558039cb4573750bfad998ffafbf59e832ef4
WTHACD <walejandrocamacho@gmail.com>
2025-02-08 01:32:55 -0600

    Holaaaa

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -1,9 +1,29 @@
 using System;
 
-class Program
+public class Program
 {
-    static void Main(string[] args)
+    public static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment assignment = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine(assignment.GetSummary());
+       
+
+        Console.WriteLine();
+
+        MathAssignment mathAssignment = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+        
+
+        Console.WriteLine(); 
+
+        WritingAssignment writingAssignment = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+
+        Console.WriteLine(writingAssignment.GetSummary());
+
+        Console.WriteLine(writingAssignment.GetWritingInformation());
+        
     }
 }
+
+//555
